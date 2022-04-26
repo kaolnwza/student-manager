2a4c75275db8e439a6d252aaacff31acea2f14ec
PwrFr <54875724+PwrFr@users.noreply.github.com>
2022-04-27 00:31:18 +0700

    Attendance Page

--- fnend/pages/home/subject/[teacherid]/attendance/[subjectid].js
+++ fnend/pages/home/subject/[teacherid]/attendance/[subjectid].js
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react"
 import { useRouter } from 'next/router'
-import { Tabs, Tab, Row, Col, Container } from 'react-bootstrap';
+import { Tabs, Tab, Row, Col, Container, Nav } from 'react-bootstrap';
 
 export const getServerSideProps = async (ctx) => {
     const resClass = await fetch('http://localhost:3001/util/getarraybyany/class/subject_id/' + ctx.query.subjectid)
     const classes = await resClass.json()
-    // const resAtten = await fetch(`http://localhost:3001/util/getarraybyany/class_attendance/class_id/62002`)
-    // const atten = await resAtten.json()
-    // console.log(atten);
+
     return {
         props: {
             classes: classes
@@ -16,16 +14,18 @@ export const getServerSideProps = async (ctx) => {
 }
 const attendance = ({ classes }) => {
     const [key, setKey] = useState(classes[0].class_id);
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+
+    const [week, setWeek] = useState([''])
+
+    const [atdid, setAtdid] = useState(0);
+    const [studentlist, setList] = useState([])
 
     useEffect(() => {
         // declare the data fetching function
         const fetchData = async () => {
             const resAtten = await fetch(`http://localhost:3001/util/getarraybyany/class_attendance/class_id/${key}`);
             const atten = await resAtten.json()
-            setData(atten)
+            setWeek(atten)
         }
 
         // call the function
@@ -34,8 +34,16 @@ const attendance = ({ classes }) => {
             .catch(console.error);
     }, [key])
 
+
+    const fetchStudent = async (id) => {
+        const resStudent = await fetch(`http://localhost:3001/util/getarraybyany/student_attendance/attendance_id/${id}`);
+        const student = await resStudent.json()
+        setList(student)
+        console.log(student);
+    }
+
     return (
-        <div className="d-flex justify-content-center w-75">
+        <div className="d-flex justify-content-center w-75 h-50">
 
             <Container >
 
@@ -47,15 +55,73 @@ const attendance = ({ classes }) => {
                 >
                     {classes.map((cls, i) =>
                         <Tab key={i} eventKey={cls.class_id} title={`Sec ${i + 1}`} >
-                            {data.map((at, index) => <p>
-                                {at.class_id}
 
-                                {at.attendance_name}
-                            </p>)}
+                            <Tab.Container
+                                activeKey={atdid}
+                                onSelect={(k) => setAtdid(k)}
+                                id="left-tabs-example"
+                                defaultActiveKey="first">
+                                <Row>
+                                    <Col sm={3}>
+                                        <Nav variant="pills" className="flex-column">
+                                            {week.map((week, j) =>
+                                                <Nav.Item key={j} onClick={() =>
+                                                    fetchStudent(week.attendance_id)
+                                                }>
+
+
+                                                    <Nav.Link eventKey={week.attendance_id}>{week.attendance_name}</Nav.Link>
+                                                </Nav.Item>
+                                            )}
+                                        </Nav>
+                                    </Col>
+                                    <Col sm={9}>
+                                        <Tab.Content>
+                                            {week.map((week, x) =>
+
+                                                <Tab.Pane key={x} eventKey={week.attendance_id} >
+                                                    {studentlist.map(person => <ul>
+                                                        <li>{person.student_id} | {person.attendance_status == 'come' ?
+                                                            <lord-icon
+                                                                src="https://cdn.lordicon.com/hjeefwhm.json"
+                                                                trigger="morph"
+                                                                style={{ height: '2rem', width: '2rem' }}
+                                                            >
+                                                            </lord-icon>
+                                                            :
+                                                            (person.attendance_status == 'notcome' ?
+                                                                <lord-icon
+                                                                    src="https://cdn.lordicon.com/vfzqittk.json"
+                                                                    trigger="morph"
+                                                                >
+                                                                </lord-icon>
+                                                                :
+                                                                <lord-icon
+                                                                    src="https://cdn.lordicon.com/abgtphux.json"
+                                                                    trigger="morph"
+                                                                    style={{ height: '2rem', width: '2rem' }}
+                                                                >
+                                                                </lord-icon>
+                                                            )
+                                                        }
+                                                        </li>
+                                                    </ul>
+                                                    )}
+
+                                                </Tab.Pane>
+                                            )}
+                                        </Tab.Content>
+
+                                    </Col>
+                                </Row>
+                            </Tab.Container>
+
 
 
                         </Tab>
                     )}
+
+
                 </Tabs>
             </Container>
         </div>
