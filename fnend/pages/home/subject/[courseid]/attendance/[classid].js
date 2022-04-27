e5ed178cf0e0f9180931364b477100d942d302c6
PwrFr <54875724+PwrFr@users.noreply.github.com>
2022-04-27 20:43:32 +0700

    Attendance Page
    Finish Add Attendance

--- fnend/pages/home/subject/[courseid]/attendance/[classid].js
+++ fnend/pages/home/subject/[courseid]/attendance/[classid].js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { useRouter } from 'next/router'
-import { Tabs, Tab, Row, Col, Container, Nav } from 'react-bootstrap';
+import { Tabs, Tab, Button, Modal, Container, Table, InputGroup, FormControl } from 'react-bootstrap';
 
 export const getServerSideProps = async (ctx) => {
     const resClass = await fetch('http://localhost:3001/attendance/class/' + ctx.query.classid)
@@ -14,116 +14,152 @@ export const getServerSideProps = async (ctx) => {
 }
 const attendance = ({ classes }) => {
     const [key, setKey] = useState(0);
+    const [show, setShow] = useState(false);
+    const [form, setForm] = useState('');
 
-    const [week, setWeek] = useState([''])
-
-    const [atdid, setAtdid] = useState(0);
-    const [studentlist, setList] = useState([])
-
-    // useEffect(() => {
-    //     // declare the data fetching function
-    //     const fetchData = async () => {
-    //         const resAtten = await fetch(`http://localhost:3001/attendance/class/${key}`);
-    //         const atten = await resAtten.json()
-    //         console.log(atten);
-    //         setWeek(atten)
-    //     }
-
-    //     // call the function
-    //     fetchData()
-    //         // make sure to catch any error
-    //         .catch(console.error);
-    // }, [key])
-
-
-    const fetchStudent = async (id) => {
-        const resStudent = await fetch(`http://localhost:3001/util/getarraybyany/student_attendance/attendance_id/${id}`);
-        const student = await resStudent.json()
-        console.log(student);
-
-        setList(student)
-    }
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
-        <div className="d-flex justify-content-center w-75 h-50">
+        <div className="d-flex justify-content-center w-75 h-75">
 
             <Container >
+                <a class="btn text-center w-100" onClick={handleShow}>
+                    <lord-icon
+                        target="a.btn"
+                        src="https://cdn.lordicon.com/auvicynv.json"
+                        trigger="morph"
+                        style={{ height: '3rem', width: '3rem' }}
+                    >
+                    </lord-icon>
+                    <p>Add Attendance</p>
+                </a>
 
                 <Tabs
                     activeKey={key}
                     onSelect={(k) => setKey(k)}
-                    className="mb-3   justify-content-center "
-                // onClick={() => fetchMyAPI()}
+                    className="mb-3 justify-content-center "
                 >
 
                     {classes.map((cls, i) =>
-                        <Tab key={i} eventKey={i} title={`${cls[i].attendance_id}`} >
-                            {/* {cls} */}
-                            {/* <Tab.Container
-                                activeKey={atdid}
-                                onSelect={(k) => setAtdid(k)}
-                                id="left-tabs-example"
-                                defaultActiveKey="first">
-                                <Row>
-                                    <Col sm={3}>
-                                        <Nav variant="pills" className="flex-column">
-                                            {week.map((week, j) =>
-                                                <Nav.Item key={j}>
-                                                    <Nav.Link eventKey={week.attendance_id}>{week.attendance_name}</Nav.Link>
-                                                </Nav.Item>
-                                            )}
-                                        </Nav>
-                                    </Col>
-                                    <Col sm={9}>
-                                        <Tab.Content>
-                                            {week.map((week, x) =>
 
-                                                <Tab.Pane key={x} eventKey={week.attendance_id} >
-                                                    {studentlist.map(person => <ul>
-                                                        <li>{person.student_id} | {person.attendance_status == 'come' ?
-                                                            <lord-icon
-                                                                src="https://cdn.lordicon.com/hjeefwhm.json"
-                                                                trigger="morph"
-                                                                style={{ height: '2rem', width: '2rem' }}
-                                                            >
-                                                            </lord-icon>
-                                                            :
-                                                            (person.attendance_status == 'notcome' ?
-                                                                <lord-icon
-                                                                    src="https://cdn.lordicon.com/vfzqittk.json"
-                                                                    trigger="morph"
-                                                                >
-                                                                </lord-icon>
-                                                                :
-                                                                <lord-icon
-                                                                    src="https://cdn.lordicon.com/abgtphux.json"
-                                                                    trigger="morph"
-                                                                    style={{ height: '2rem', width: '2rem' }}
-                                                                >
-                                                                </lord-icon>
-                                                            )
-                                                        }
-                                                        </li>
-                                                    </ul>
-                                                    )}
+                        <Tab key={i} eventKey={i} title={`${cls[i].attendance_name}`} style={{ height: '40vh', overflowY: 'scroll' }}>
 
-                                                </Tab.Pane>
-                                            )}
-                                        </Tab.Content>
+                            <Table borderless hover className="text-center" >
+                                <thead>
+                                    <tr >
+                                        <th>Id</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {cls.map((person, j) =>
+                                        <tr>
+                                            <td>{person.student_id} </td>
+                                            <td>{person.student_firstname}</td>
+                                            <td>{person.student_lastname}</td>
+                                            <td>
+                                                {person.attendance_status == 'come' ?
+                                                    <lord-icon
+                                                        src="https://cdn.lordicon.com/hjeefwhm.json"
+                                                        trigger="morph"
+                                                        style={{ height: '2rem', width: '2rem' }}
+                                                    >
+                                                    </lord-icon>
+                                                    :
+                                                    (person.attendance_status == 'notcome' ?
+                                                        <lord-icon
+                                                            src="https://cdn.lordicon.com/vfzqittk.json"
+                                                            trigger="morph"
+                                                        >
+                                                        </lord-icon>
+                                                        :
+                                                        <lord-icon
+                                                            src="https://cdn.lordicon.com/abgtphux.json"
+                                                            trigger="morph"
+                                                            style={{ height: '2rem', width: '2rem' }}
+                                                        >
+                                                        </lord-icon>
+                                                    )
+                                                }
 
-                                    </Col>
-                                </Row>
-                            </Tab.Container> */}
+                                            </td>
+                                        </tr>
+                                    )}
 
-
-
+                                </tbody>
+                            </Table>
                         </Tab>
                     )}
-                    {/* {key} */}
 
                 </Tabs>
+                <a class="btn text-center w-100">
+                    <lord-icon
+                        target="a.btn"
+                        src="https://cdn.lordicon.com/wloilxuq.json"
+                        trigger="morph"
+                        style={{ height: '4rem', width: '4rem' }}
+                    >
+                    </lord-icon>
+                    <p>Edit Attendance</p>
+                </a>
             </Container>
-        </div>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                // centered
+                style={{ fontFamily: 'roboto slab' }}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Attendance</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <InputGroup className="mb-3">
+                        <FormControl
+                            placeholder="Attendance"
+                            value={form}
+                            onChange={(e) => setForm(e.target.value)}
+                        />
+
+                    </InputGroup>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" className="py-1 btn" onClick={() => {
+                        setTimeout(() => {
+                            setShow(false)
+                        }, 50)
+                    }}>
+                        Cancel
+                        <lord-icon
+                            src="https://cdn.lordicon.com/eflfmgmj.json"
+                            trigger="click"
+                            target="Button.btn"
+                            colors="primary:#fff"
+                            state="hover-1"
+                            style={{ height: '1.5rem', width: '1.5rem' }}>
+                        </lord-icon>
+                    </Button>
+                    <Button className="btn py-0" variant="light" onClick={() => {
+                        setTimeout(() => {
+                            setShow(false)
+                        }, 1500)
+                    }}>
+                        Add
+                        <lord-icon
+                            src="https://cdn.lordicon.com/mecwbjnp.json"
+                            target="Button.btn"
+                            trigger="click"
+                            style={{ height: '2rem', width: '2rem' }}>
+                        </lord-icon>
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div >
 
     );
 }
