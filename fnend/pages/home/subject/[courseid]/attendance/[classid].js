2f30eef5188b8953f773ebfb5c642242c735eb59
PwrFr <54875724+PwrFr@users.noreply.github.com>
2022-04-29 17:35:15 +0700

    refactor Update Status in Attendance Page

--- fnend/pages/home/subject/[courseid]/attendance/[classid].js
+++ fnend/pages/home/subject/[courseid]/attendance/[classid].js
@@ -16,10 +16,6 @@ const attendance = ({ classes }) => {
     const [key, setKey] = useState(0);
     const [show, setShow] = useState(false);
     const [form, setForm] = useState('');
-    const [selectWeek, setSelectWeek] = useState(0);
-    const [selectStudent, setSelectStudent] = useState(0);
-    const [radioValue, setRadioValue] = useState(0);
-    const [edit, setEdit] = useState(false);
     const [expandedId, setExpandedId] = useState(-1);
     const [attendanceId, setAttendanceId] = useState(0)
     const handleExpandClick = (i) => {
@@ -55,8 +51,6 @@ const attendance = ({ classes }) => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const handleEditClose = () => setEdit(false);
-    const handleEditShow = () => setEdit(true);
     const addAttendance = () => {
         const data = {
             class_id: rounter.query.classid,
@@ -68,20 +62,13 @@ const attendance = ({ classes }) => {
     };
     const EditStudent = (status, student) => {
         const data = {
+            class_id: rounter.query.classid,
             attendance_id: attendanceId,
             student_id: student,
             attendance_status: status,
         }
-        // setSelectWeek(0)
-        // setSelectStudent(0)
-        // setRadioValue(0)
-        // setEdit(false)
-        console.log(student);
-        console.log(status);
-        console.log(attendanceId);
-        setRadioValue(status)
 
-        // setRadioValue(status)
+        console.log(data);
 
     }
 
@@ -118,7 +105,6 @@ const attendance = ({ classes }) => {
                                         <th>First Name</th>
                                         <th>Last Name</th>
                                         <th>Status</th>
-                                        {/* <th>Edit</th> */}
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -127,14 +113,13 @@ const attendance = ({ classes }) => {
                                             <td>{person.student_id} </td>
                                             <td>{person.student_firstname}</td>
                                             <td>{person.student_lastname}</td>
-                                            <td>
+                                            <td className="w-25 p-0 m-0">
                                                 {expandedId !== j ?
-
                                                     person.attendance_status == 'come' ?
                                                         <lord-icon
                                                             src="https://cdn.lordicon.com/hjeefwhm.json"
                                                             trigger="morph"
-                                                            style={{ height: '2rem', width: '2rem' }}
+                                                            style={{ cursor: 'pointer' }}
                                                             onClick={() => handleExpandClick(j)}
                                                         >
                                                         </lord-icon>
@@ -144,23 +129,24 @@ const attendance = ({ classes }) => {
                                                                 src="https://cdn.lordicon.com/vfzqittk.json"
                                                                 trigger="morph"
                                                                 onClick={() => handleExpandClick(j)}
+                                                                style={{ cursor: 'pointer' }}
+
                                                             >
                                                             </lord-icon>
                                                             :
                                                             <lord-icon
                                                                 src="https://cdn.lordicon.com/abgtphux.json"
                                                                 trigger="morph"
-                                                                style={{ height: '2rem', width: '2rem' }}
+                                                                style={{ cursor: 'pointer' }}
                                                                 onClick={() => handleExpandClick(j)}
                                                             >
                                                             </lord-icon>
                                                         )
-
-
                                                     :
                                                     <ButtonGroup >
                                                         {status.map((radio, idx) => (
                                                             <ToggleButton
+                                                                className="p-0"
                                                                 key={idx}
                                                                 id={`radio-${idx}`}
                                                                 type="radio"
@@ -172,7 +158,7 @@ const attendance = ({ classes }) => {
                                                                     handleExpandClick(j)
                                                                     EditStudent(e.currentTarget.value, person.student_id)
                                                                 }}
-                                                                onClick={() => {
+                                                                onClick={(e) => {
                                                                     setAttendanceId(person.attendance_id)
                                                                 }}
                                                             >
@@ -183,40 +169,12 @@ const attendance = ({ classes }) => {
                                                         ))}
 
                                                     </ButtonGroup>}
-                                                {/* {radioValue} */}
-
                                             </td>
-                                            {/* <td>
-                                                <Button className="btn py-0 px-0" variant="">
-                                                    <lord-icon
-                                                        src="https://cdn.lordicon.com/wloilxuq.json"
-                                                        target="Button.btn"
-                                                        trigger="click"
-                                                        state="hover-2"
-                                                        style={{ height: '2.5rem', width: '2.5rem' }}>
-                                                    </lord-icon>
-                                                </Button>
-                                            </td> */}
-
-                                        </tr>
-                                    )}
-
+                                        </tr>)}
                                 </tbody>
                             </Table>
-                        </Tab>
-                    )}
-
+                        </Tab>)}
                 </Tabs>
-                {/* <a class="btn " onClick={handleEditShow}>
-                    <lord-icon
-                        target="a.btn"
-                        src="https://cdn.lordicon.com/wloilxuq.json"
-                        trigger="morph"
-                        style={{ height: '4rem', width: '4rem' }}
-                    >
-                    </lord-icon>
-                    <p>Edit Attendance</p>
-                </a> */}
             </Container>
 
             {/* Add Modal */}
@@ -225,7 +183,6 @@ const attendance = ({ classes }) => {
                 onHide={handleClose}
                 backdrop="static"
                 keyboard={false}
-                // centered
                 style={{ fontFamily: 'roboto slab' }}
             >
                 <Modal.Header closeButton>
@@ -275,110 +232,6 @@ const attendance = ({ classes }) => {
                 </Modal.Footer>
             </Modal>
 
-            {/* Edit Modal */}
-            <Modal
-                show={edit}
-                onHide={handleEditClose}
-                backdrop="static"
-                keyboard={false}
-                style={{ fontFamily: 'roboto slab' }}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>Edit Attendance</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form.Group controlId="formBasicSelect">
-
-
-                        <Form.Label>Select Week | attendance_id : {selectWeek} </Form.Label>
-                        <Form.Select
-                            value={selectWeek}
-                            onChange={e => {
-                                setSelectWeek(e.target.value);
-                            }}
-                        >
-                            <option value={0} disabled>--select--</option>
-
-                            {classes.map((cls, i) => <>
-                                {/* {console.log(cls[i].attendance_name)} */}
-                                <option value={cls[i].attendance_id}> {cls[i].attendance_name} </option>
-                            </>
-                            )}
-
-                        </Form.Select>
-
-
-                        <Form.Label className="mt-3">Select Student | Student ID : {selectStudent} </Form.Label>
-                        <Form.Select
-                            value={selectStudent}
-                            onChange={e => {
-                                setSelectStudent(e.target.value);
-                            }}
-                        >
-                            <option value={0} disabled>--select--</option>
-
-                            {classes[0].map((student, i) => <>
-                                {/* {console.log(student[i].attendance_name)} */}
-                                <option value={student.student_id}>{student.student_id} {student.student_firstname} {student.student_lastname} </option>
-                            </>
-                            )}
-
-                        </Form.Select>
-
-                        <Form.Label className="mt-3">Select Status | Status : {radioValue} </Form.Label><br />
-                        {/* <ButtonGroup>
-                            {status.map((radio, idx) => (
-                                <ToggleButton
-                                    key={idx}
-                                    id={`radio-${idx}`}
-                                    type="radio"
-                                    variant={radio.color}
-                                    name="radio"
-                                    value={radio.value}
-                                    checked={radioValue === radio.value}
-                                    onChange={(e) => setRadioValue(e.currentTarget.value)}
-                                >
-                                    {radio.name}
-                                </ToggleButton>
-                            ))}
-                        </ButtonGroup> */}
-                    </Form.Group>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="light" className="py-1 btn" onClick={() => {
-                        setTimeout(() => {
-                            setEdit(false)
-                            setSelectWeek(0)
-                            setSelectStudent(0)
-                            setRadioValue(0)
-                        }, 50)
-                    }}>
-                        Cancel
-                        <lord-icon
-                            src="https://cdn.lordicon.com/eflfmgmj.json"
-                            trigger="click"
-                            target="Button.btn"
-                            colors="primary:#000"
-                            state="hover-1"
-                            style={{ height: '1.5rem', width: '1.5rem' }}>
-                        </lord-icon>
-                    </Button>
-                    <Button className="btn py-0  border-success" variant="" onClick={() => {
-                        setTimeout(() => {
-                            // EditStudent()
-                        }, 1000)
-                    }}>
-                        Edit
-                        <lord-icon
-                            src="https://cdn.lordicon.com/wloilxuq.json"
-                            target="Button.btn"
-                            trigger="click"
-                            state="hover-2"
-                            style={{ height: '2.5rem', width: '2.5rem' }}>
-                        </lord-icon>
-                    </Button>
-                </Modal.Footer>
-            </Modal>
         </div >
 
     );
